1604de399fcf9593995d684e6162e4cb7fe811c6
0ne <sebmcke@gmail.com>
2025-08-25 14:19:16 -0600

    Add move validation for Queen Piece

--- spec/chess_api_0ne/pieces/queen_spec.rb
+++ spec/chess_api_0ne/pieces/queen_spec.rb
@@ -15,5 +15,38 @@ RSpec.describe ChessApi0ne::Pieces::Queen do
                 expect(@queen).to be_a(ChessApi0ne::Pieces::Queen)
             end
         end
+
+        describe "#move" do
+            it "should be able to move freely along ranks" do
+                expect(@queen.move("d8")).to be(true)
+                expect(@queen.position).to eq("d8")
+            end
+
+            it "should be able to move freely along files" do
+                expect(@queen.move("a1")).to be(true)
+                expect(@queen.position).to eq("a1")
+            end
+
+            it "should be able to move freely along diagonals" do
+                expect(@queen.move("f3")).to be(true)
+                expect(@queen.position).to eq("f3")
+
+                expect(@queen.move("d5")).to be(true)
+                expect(@queen.position).to eq("d5")
+            end
+
+            it "should not be able to make illegal moves" do
+                expect(@queen.move("e3")).to be(false)
+                expect(@queen.position).to eq("d1")
+
+                expect(@queen.move("c5")).to be(false)
+                expect(@queen.position).to eq("d1")
+            end
+
+            it "should not be able to move on top of itself" do
+                expect(@queen.move("d1")).to be(false)
+                expect(@queen.position).to eq("d1")
+            end
+        end
     end
 end
