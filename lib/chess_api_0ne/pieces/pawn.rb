2745d2c49c214c63a547c978a239ec260d34b8c6
0ne <sebmcke@gmail.com>
2025-08-25 15:11:45 -0600

    Create move validation for pawns

--- lib/chess_api_0ne/pieces/pawn.rb
+++ lib/chess_api_0ne/pieces/pawn.rb
@@ -1,5 +1,24 @@
 module ChessApi0ne::Pieces
     class Pawn < ChessApi0ne::Piece
+        def move(position)
+            current_pos = @position.chars
+            current_pos[1] = current_pos[1].to_i
+            current_pos[0] = current_pos[0].ord
 
+            move_pos = position.chars
+            move_pos[1] = move_pos[1].to_i
+            move_pos[0] = move_pos[0].ord
+
+            if @color == :white
+                return false if !(move_pos[1] > current_pos[1])
+                return false if (move_pos[1] - current_pos[1]) > 1 && !(current_pos[1] == 2)
+            elsif @color == :black
+                return false if !(move_pos[1] < current_pos[1])
+                return false if -(move_pos[1] - current_pos[1]) > 1 && !(current_pos[1] == 7)
+            end
+
+            @position = position
+            true
+        end
     end
 end
--- spec/chess_api_0ne/pieces/pawn_spec.rb
+++ spec/chess_api_0ne/pieces/pawn_spec.rb
@@ -13,5 +13,67 @@ RSpec.describe ChessApi0ne::Pieces::Pawn do
                 expect(@pawn).to be_a(ChessApi0ne::Pieces::Pawn)
             end
         end
+
+        describe "#move" do
+            describe "white" do 
+                it "should be able to move one square forward" do
+                    expect(@pawn.move("e3")).to be(true)
+                    expect(@pawn.position).to eq("e3")
+                end
+    
+                it "should be able to move two squares forward on its first move" do
+                    expect(@pawn.move("e4")).to be(true)
+                end
+    
+                it "should be able to capture along diagonals" do
+                    expect(@pawn.move("f3")).to be(true)
+                    expect(@pawn.move("e4")).to be(true)
+                end
+    
+                it "should only be able to move forwards" do
+                    expect(@pawn.move("f2")).to be(false)
+                    expect(@pawn.move("e1")).to be(false)
+                    expect(@pawn.move("d1")).to be(false)
+                    expect(@pawn.position).to eq("e2")
+                end
+    
+                it "should only be able to move one square after its first move" do
+                    expect(@pawn.move("e3")).to be(true)
+                    expect(@pawn.move("e5")).to be(false)
+                end
+            end
+
+            describe "black" do
+                before :each do
+                    @pawn = ChessApi0ne::Pieces::Pawn.new("e7", :black) 
+                end
+
+                it "should be able to move one square forward" do
+                    expect(@pawn.move("e6")).to be(true)
+                    expect(@pawn.position).to eq("e6")
+                end
+    
+                it "should be able to move two squares forward on its first move" do
+                    expect(@pawn.move("e5")).to be(true)
+                end
+    
+                it "should be able to capture along diagonals" do
+                    expect(@pawn.move("f6")).to be(true)
+                    expect(@pawn.move("e5")).to be(true)
+                end
+    
+                it "should only be able to move forwards" do
+                    expect(@pawn.move("f7")).to be(false)
+                    expect(@pawn.move("e8")).to be(false)
+                    expect(@pawn.move("d8")).to be(false)
+                    expect(@pawn.position).to eq("e7")
+                end
+    
+                it "should only be able to move one square after its first move" do
+                    expect(@pawn.move("e6")).to be(true)
+                    expect(@pawn.move("e4")).to be(false)
+                end
+            end
+        end
     end
 end
