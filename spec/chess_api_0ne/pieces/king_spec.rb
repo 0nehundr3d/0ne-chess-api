c950e023535bb47d458337fdaf4ef16ae51d75b6
0ne <sebmcke@gmail.com>
2025-08-25 14:35:26 -0600

    Create move validation for King Piece

--- spec/chess_api_0ne/pieces/king_spec.rb
+++ spec/chess_api_0ne/pieces/king_spec.rb
@@ -15,5 +15,45 @@ RSpec.describe ChessApi0ne::Pieces::King do
                 expect(@king).to be_a(ChessApi0ne::Pieces::King)
             end
         end
+
+        describe "#move" do
+            it "should be able to move one square along ranks" do
+                expect(@king.move("e2")).to be(true)
+                expect(@king.position).to eq("e2")
+            end
+
+            it "should be able to move one square along files" do
+                expect(@king.move("d1")).to be(true)
+                expect(@king.position).to eq("d1")
+            end
+
+            it "should be able to move one square along diagonals" do
+                expect(@king.move("f2")).to be(true)
+                expect(@king.position).to eq("f2")
+
+                expect(@king.move("e3")).to be(true)
+                expect(@king.position).to eq("e3")
+            end
+
+            it "should not be able to make illegal moves" do
+                expect(@king.move("d3")).to be(false)
+                expect(@king.position).to eq("e1")
+
+                expect(@king.move("c5")).to be(false)
+                expect(@king.position).to eq("e1")
+            end
+
+            it "should only be able to move one square in any direction" do
+                expect(@king.move("e3")).to be(false)
+                expect(@king.move("g1")).to be(false)
+                expect(@king.move("c3")).to be(false)
+                expect(@king.position).to eq("e1")
+            end
+
+            it "should not be able to move on top of itself" do
+                expect(@king.move("e1")).to be(false)
+                expect(@king.position).to eq("e1")
+            end
+        end
     end
 end
