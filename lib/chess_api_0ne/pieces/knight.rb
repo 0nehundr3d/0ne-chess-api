45d1b93eb5f41473379da0f36952d9a8bb9796dc
0ne <sebmcke@gmail.com>
2025-08-25 13:31:55 -0600

    Create validation for Knight movement

--- lib/chess_api_0ne/pieces/knight.rb
+++ lib/chess_api_0ne/pieces/knight.rb
@@ -1,4 +1,22 @@
 module ChessApi0ne::Pieces
     class Knight < ChessApi0ne::Piece
+        def move(position)
+            current_pos = @position.chars
+            current_pos[1] = current_pos[1].to_i
+            current_pos[0] = current_pos[0].ord
+
+            move_pos = position.chars
+            move_pos[1] = move_pos[1].to_i
+            move_pos[0] = move_pos[0].ord
+
+            binding.pry if position == "c5"
+            return false if (move_pos[1] - current_pos[1]).abs > 2
+            return false if (move_pos[0] - current_pos[0]).abs > 2
+            return false if (move_pos[0] - current_pos[0]).abs != 1 && (move_pos[1] - current_pos[1]).abs != 1
+            return false if (move_pos[0] - current_pos[0]).abs != 2 && (move_pos[1] - current_pos[1]).abs != 2
+
+            @position = position
+            true
+        end
     end
 end
--- spec/chess_api_0ne/pieces/knight_spec.rb
+++ spec/chess_api_0ne/pieces/knight_spec.rb
@@ -7,12 +7,47 @@ RSpec.describe ChessApi0ne::Pieces::Knight do
 
     describe "Instance methods" do
         before :each do
-            @knight = ChessApi0ne::Pieces::Knight.new("a2")
+            @knight = ChessApi0ne::Pieces::Knight.new("b1")
         end
 
         describe "#initialize" do
             it "should exist" do
                 expect(@knight).to be_a(ChessApi0ne::Pieces::Knight)
+            end
+        end
+
+        describe "#move" do
+            it "should be able to move two squares in one direction and one in another" do
+                expect(@knight.move("c3")).to be(true)
+                expect(@knight.position).to eq("c3")
+            end
+
+            it "should not be able to move more than two squares in any direction" do
+                expect(@knight.move("c4")).to be(false)
+                expect(@knight.move("e2")).to be(false)
+                expect(@knight.position).to eq("b1")
+            end
+
+            it "should not be able to move more than two squares backwards" do
+                knight = ChessApi0ne::Pieces::Knight.new("g8", :black)
+
+                expect(knight.move("h5")).to be(false)
+                expect(knight.move("d7")).to be(false)
+            end
+
+            it "should be required to move at least one square in one direction" do
+                expect(@knight.move("b3")).to be(false)
+                expect(@knight.move("c3")).to be(true)
+                expect(@knight.position).to eq("c3")
+                
+                expect(@knight.move("e3")).to be(false)
+                expect(@knight.move("e4")).to be(true)
+                expect(@knight.position).to eq("e4")
+            end
+
+            it "should be required to move at least two squares in one direction" do
+                expect(@knight.move("c2")).to be(false)
+                expect(@knight.position).to eq("b1")
             end
         end
     end
