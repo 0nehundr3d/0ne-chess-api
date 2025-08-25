8fa429e79995a5d784c24aefbd69e6d021cf29bb
0ne <sebmcke@gmail.com>
2025-08-25 11:19:06 -0600

    Remove initial test

--- spec/chess_api_0ne_spec.rb
+++ spec/chess_api_0ne_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ChessApi0ne do
   it "has a version number" do
     expect(ChessApi0ne::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
