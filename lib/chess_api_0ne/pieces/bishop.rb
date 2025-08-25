0f948e070faa255e26748744e1da3feb792ecc17
0ne <sebmcke@gmail.com>
2025-08-25 14:03:06 -0600

    Add move validation for bishops

--- lib/chess_api_0ne/pieces/bishop.rb
+++ lib/chess_api_0ne/pieces/bishop.rb
@@ -1,7 +1,18 @@
 module ChessApi0ne::Pieces
-    class Bishop
-        def initialize(position)
+    class Bishop < ChessApi0ne::Piece
+        def move(position)
+            current_pos = @position.chars
+            current_pos[0] = current_pos[0].ord
+            current_pos[1] = current_pos[1].to_i
 
+            move_pos = position.chars
+            move_pos[0] = move_pos[0].ord
+            move_pos[1] = move_pos[1].to_i
+
+            return false if (move_pos[1] - current_pos[1]).abs != (move_pos[0] - current_pos[0]).abs
+
+            @position = position
+            true
         end
     end
 end
