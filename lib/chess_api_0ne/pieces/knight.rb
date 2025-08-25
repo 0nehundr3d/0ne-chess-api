d101fa4e60dbdbc27e9bb4a0a4bceb3bd5ca8d65
0ne <sebmcke@gmail.com>
2025-08-25 13:35:21 -0600

    refactor knight bound checking into one line

--- lib/chess_api_0ne/pieces/knight.rb
+++ lib/chess_api_0ne/pieces/knight.rb
@@ -9,9 +9,7 @@ module ChessApi0ne::Pieces
             move_pos[1] = move_pos[1].to_i
             move_pos[0] = move_pos[0].ord
 
-            binding.pry if position == "c5"
-            return false if (move_pos[1] - current_pos[1]).abs > 2
-            return false if (move_pos[0] - current_pos[0]).abs > 2
+            return false if (move_pos[1] - current_pos[1]).abs > 2 || (move_pos[0] - current_pos[0]).abs > 2
             return false if (move_pos[0] - current_pos[0]).abs != 1 && (move_pos[1] - current_pos[1]).abs != 1
             return false if (move_pos[0] - current_pos[0]).abs != 2 && (move_pos[1] - current_pos[1]).abs != 2
 
